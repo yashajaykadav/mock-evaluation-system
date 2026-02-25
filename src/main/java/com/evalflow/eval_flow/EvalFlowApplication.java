d8df0c726a093d8ebf396b5a1b89fb611e484d23
YASHKADAV <110305731+yashajaykadav@users.noreply.github.com>
2026-02-25 23:19:53 +0530

    Repo Created

---
+++ src/main/java/com/evalflow/eval_flow/EvalFlowApplication.java
@@ -0,0 +1,13 @@
+package com.evalflow.eval_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvalFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evalflow/eval_flow/EvalFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evalflow.eval_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
